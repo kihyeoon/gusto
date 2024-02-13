9ea257c3df715c707ac97d47c43ff161971d2734
kihyeoon <bangion93@gmail.com>
2024-02-14 00:38:51 +0900

    ✨ 레시피 요청시 로딩 UI 구현

--- src/components/VideoRecipe.tsx
+++ src/components/VideoRecipe.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import { ReloadIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 
+import RecipeSkeleton from "@/components/RecipeSkeleton";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 import { type Script } from "@/app/api/recipe/video/[id]/route";
 
 export default function VideoRecipe() {
-  const [videoId, setVideoId] = useState<string>("");
+  const [videoId, setVideoId] = useState("");
   const [recipe, setRecipe] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = async () => {
+    setLoading(true);
     const script = await fetch(`/api/recipe/video/${videoId}`).then((res) =>
       res.json(),
     );
@@ -28,23 +32,36 @@ export default function VideoRecipe() {
       );
 
     setRecipe(recipe);
+    setLoading(false);
   };
 
+  const isReady = !loading && recipe.length > 0;
+
   return (
-    <div className="flex flex-col gap-7">
-      <div className="flex w-96 items-center justify-center gap-3">
+    <div className="flex w-full flex-col gap-7">
+      <div className="flex w-full items-center justify-center gap-3">
         <Input
           className="w-full"
           value={videoId}
           onChange={(e) => setVideoId(e.target.value)}
           placeholder="Enter a YouTube video URL"
         />
-        <Button onClick={handleClick}>Submit</Button>
+        <Button onClick={handleClick} disabled={loading}>
+          {loading ? (
+            <>
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+              please wait
+            </>
+          ) : (
+            "Create Recipe"
+          )}
+        </Button>
       </div>
+      {loading && <RecipeSkeleton />}
       <ul>
-        {recipe &&
+        {isReady &&
           recipe.map((step, index) => (
-            <div key={index} className="text-lg">
+            <div key={index} className="">
               {step}
             </div>
           ))}
