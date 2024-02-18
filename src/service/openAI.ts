58785a10c539a5929f3556181a4175ddbb63703c
kihyeoon <bangion93@gmail.com>
2024-02-19 01:13:12 +0900

    ✨ 레시피 생성 ai 프롬프트 엔지니어링 개선 - JSON 형태로 변경

--- src/service/openAI.ts
+++ src/service/openAI.ts
@@ -9,8 +9,13 @@ export class OpenAIService {
   private messages: OpenAI.ChatCompletionMessageParam[] = [
     {
       role: "system",
-      content:
-        "당신은 세계 최고의 요리 레시피 생성봇 입니다. 당신은 어떤 요리에 관한 설명이든 상세하게 잘 정리된 레시피로 바꿀 수 있습니다. 유튜브 요리 영상의 자막을 입력하면, 당신은 요리 레시피에 관한 정보만 추출해서 한국어 레시피만 반환합니다. 정확한 재료의 양을 측정할 수 있다면 기입해 주고 만약에 자막의 설명만으로 부족한 내용이 있다면 일반적인 레시피를 기반으로 해서 유추하여 내용을 채워주세요. 만약 요리에 관련된 영상의 자막이 아니라면 요리 영상이 아니라고 알려줍니다. 레시피만 알려주고 추가적인 미사여구는 제외하세요. 본문은 재료, 요리 순서, 팁으로만 구성해서 알려주세요. 당신은 할 수 있습니다!",
+      content: `당신은 세계 최고의 요리 레시피 제작자입니다. 당신에게는 유튜브 요리 영상의 자막이 주어집니다. 당신의 임무는 아래 규칙에 따라 자막을 분석 및 요약해서 JSON 형태의 레시피를 반환하는 것입니다. 당신은 할 수 있습니다!
+      - 인사말 등 요리와 관계없는 불필요한 정보를 제외하고 요리에 관한 정보만 추출해서 레시피 작성
+      - JSON의 key는 title, ingredients, body, tips로 구성(title을 제외한 나머지는 배열 형태로 구성)
+      - 재료의 양은 가능한 한 정확하게 측정하여 기재
+      - 만약 자막의 설명만으로 요리를 완성하기에 부족한 부분이 있다면 일반적인 레시피를 기반으로 해서 유추하여 내용을 추가
+      - 완성된 레시피에서 등장하는 재료 이름 등의 단어가 실제로 존재하지 않는 단어라면 옳은 단어를 추정해서 교체
+      - 만약 자막에 요리에 관련된 내용이 없다면 {"error":"not recipe"}를 반환`,
     },
   ];
 
@@ -25,13 +30,14 @@ export class OpenAIService {
 
   public async getChatResponse(message: string) {
     return await this.openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-0125",
+      response_format: { type: "json_object" },
       // stream: true,
       messages: [
         ...this.messages,
         {
           role: "user",
-          content: `다음은 유튜브 자막입니다. 레시피를 만들어주세요: ${message}`,
+          content: `${message}`,
         },
       ],
     });
