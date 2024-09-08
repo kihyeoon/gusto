0de3ff0cd94b5cfca1945173b00113a5a51d1146
kihyeoon <bangion93@gmail.com>
2024-09-08 12:26:10 +0900

    💄 경로명이 "/auth/signIn"일 때 렌더링 NavBar를 렌더링하지 않음 #22

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -8,6 +9,11 @@ import { Button } from "@/components/ui/button";
 export default function NavBar() {
   const { data: session } = useSession();
   const user = session?.user;
+  const pathname = usePathname();
+
+  if (pathname === "/auth/signIn") {
+    return null;
+  }
 
   return (
     <nav className="z-20 mx-auto flex h-12 w-full max-w-md items-center justify-end gap-1 bg-background px-4 pt-2">
