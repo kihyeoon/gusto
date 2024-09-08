79e3e2893e4a7f39213e23b9d03d90f2836ea5e2
kihyeoon <bangion93@gmail.com>
2024-09-08 11:10:42 +0900

    💄 레시피 썸네일 스켈레톤 UI 넓이 변경 #22

--- src/features/recipe/components/detail/RecipeSkeleton.tsx
+++ src/features/recipe/components/detail/RecipeSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function RecipeSkeleton() {
   return (
     <div className="flex flex-col gap-2">
-      <Skeleton className="h-52 w-[350px]" />
+      <Skeleton className="h-52 w-full" />
       <Skeleton className="h-4 w-[50px]" />
       <Skeleton className="h-4 w-[70px]" />
       <Skeleton className="h-4 w-[60px]" />
