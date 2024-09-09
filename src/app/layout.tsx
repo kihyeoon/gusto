d069ae628e9e670f881a1101d13ef1374a3f780c
kihyeoon <bangion93@gmail.com>
2024-09-09 11:15:01 +0900

    🔍 Viewport, metadataBase 추가 #24 🔧 images config 수정 domains -> remotePatterns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Open_Sans } from "next/font/google";
 
 import NavBar from "@/components/NavBar";
@@ -15,6 +15,7 @@ import "./globals.css";
 const fontSans = Open_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://gusto-hazel.vercel.app"),
   title: {
     template: "%s | 구스토",
     default: "구스토 - 레시피 영상 요약",
@@ -40,6 +41,16 @@ export const metadata: Metadata = {
       "구스토에서 AI를 통해 요리 영상을 요약하고 레시피를 정리해보세요",
     url: "https://gusto-hazel.vercel.app",
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#f5f5f5",
+  colorScheme: "light",
+  initialScale: 1,
+  width: "device-width",
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
