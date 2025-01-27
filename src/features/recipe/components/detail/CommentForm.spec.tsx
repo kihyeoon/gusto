e58f560e579c677ce7ed4e847c2e995ee64017f9
kihyeoon <kihyeoon@gmail.com>
2025-01-27 22:35:25 +0900

    ✅ 레시피 댓글 폼 테스트 및 접근성 개선

---
+++ src/features/recipe/components/detail/CommentForm.spec.tsx
@@ -0,0 +1,57 @@
+import { screen } from "@testing-library/react";
+
+import CommentForm from "@/features/recipe/components/detail/CommentForm";
+
+import render from "@/libs/test/render";
+
+it("기본 플레이스 홀더가 노출된다.", async () => {
+  await render(<CommentForm onSubmit={() => {}} />);
+
+  const textInput = screen.getByRole("textbox", { name: "글" });
+
+  expect(textInput).toBeInTheDocument();
+});
+
+describe("버튼", () => {
+  it("기본적으로 버튼이 disabled 상태이다.", async () => {
+    await render(<CommentForm onSubmit={() => {}} />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeDisabled();
+  });
+
+  it("글을 입력하면 버튼이 enabled 상태가 된다.", async () => {
+    const { user } = await render(<CommentForm onSubmit={() => {}} />);
+
+    const textInput = screen.getByRole("textbox", { name: "글" });
+    const submitButton = screen.getByRole("button", { name: "글 작성" });
+
+    await user.type(textInput, "test");
+
+    expect(submitButton).toBeEnabled();
+  });
+});
+
+it("글을 작성하면 작성한 글이 화면에 노출된다.", async () => {
+  const { user } = await render(<CommentForm onSubmit={() => {}} />);
+
+  const textInput = screen.getByRole("textbox", { name: "글" });
+
+  await user.type(textInput, "test");
+
+  expect(textInput).toHaveValue("test");
+});
+
+it("제출 버튼을 누르면 작성한 글이 전달된다.", async () => {
+  const onSubmit = vi.fn();
+  const { user } = await render(<CommentForm onSubmit={onSubmit} />);
+
+  const textInput = screen.getByRole("textbox", { name: "글" });
+  const submitButton = screen.getByRole("button", { name: "글 작성" });
+
+  await user.type(textInput, "test");
+  await user.click(submitButton);
+
+  expect(onSubmit).toHaveBeenCalledWith("test");
+});
