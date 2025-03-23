44ba7949c3f2df772d582d0cd10ee7172ba0920b
kihyeoon <kihyeoon@gmail.com>
2025-03-24 00:04:10 +0900

    ✨ UUID 생성 기능 추가 및 관련 테스트 케이스 작성

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -11,3 +11,11 @@ export const getQueryParam = (url: string | URL, key: string) => {
   }
   return url.searchParams.get(key);
 };
+
+export function generateUUID(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
